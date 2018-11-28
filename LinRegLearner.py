4daa5e02aaa9468f5dfb0d29469d87bcb98e90f3
David Guerrero <dguerrero23@gmail.com>
2018-11-28 00:29:15 -0500

    Added comments, reorganized

--- LinRegLearner.py
+++ LinRegLearner.py
@@ -20,7 +20,7 @@ class LinRegLearner:
     '''===========================================================
 
     ==========================================================='''
-    def medPriceCalc():
+    def med_price():
         '''
             Medium Price Calculation:
             pulls high & low price as a Data Frame from DataImport
@@ -36,59 +36,65 @@ class LinRegLearner:
         medPrices=(highPrices+lowPrices)/2.0
         return medPrices
     
-    def train(self):
+    def train_data(self):
         '''
             Raw Train data as a Numpy Array, 1st 7000 points
         '''
-        medPrices = LinRegLearner.medPriceCalc()
+        medPrices = LinRegLearner.med_price()
         trainData = medPrices[:7000]
         return trainData
     
-    def test(self):
+    def test_data(self):
         '''
             Raw Test data as a Numpy Array, 2nd 7000 points
         '''
-        medPrices = LinRegLearner.medPriceCalc()
+        medPrices = LinRegLearner.med_price()
         testData = medPrices[7000:]
         return testData
         
-    def scaling(self):
+    def data_scaling(self):
         '''
-            scales train and test data to be within a region
+            scales data to be within a region. Since older
+            data tends to be closer to 0, it wont have much
+            relevance for training. So data is broken into
+            sections then its normalized
         '''
         scaler = MinMaxScaler()
-        trainData = LinRegLearner.train(self)
-        testData = LinRegLearner.test(self)
+        trainData = LinRegLearner.train_data(self)
+        testData = LinRegLearner.test_data(self)
         
-        trainData = trainData.reshape(-1,1)#-1 is unknown num of rows+
+        #Ensures 2D array. -1 is unknown num of rows
+        trainData = trainData.reshape(-1,1)
         testData = testData.reshape(-1,1)
 
-        #Train the scaler with training and smooth data
+        #number of points to analyze at a time
         smoothingWindowSize = 1625
-        '''Split data into sections. Since earlier stock data is closer to 0 & won't be
-            relevant for training. So break into sections then normalize'''
+
         for di in range (0,6500,smoothingWindowSize):
-            #scaler.fit Computes Min and Max
+            #Compute min and max to be used later for transform
             scaler.fit(trainData[di:di+smoothingWindowSize,:])
             
+            #scaling of trainData based on a 0 - 1 range
             trainData[di:di+smoothingWindowSize,:] = scaler.transform(trainData[di:di+smoothingWindowSize,:])
     
-        #Normalize last remaining data
+        #Normalize the remaining data
         scaler.fit(trainData[di+smoothingWindowSize:,:])
         trainData[di+smoothingWindowSize:,:] = scaler.transform(trainData[di+smoothingWindowSize:,:])
         
         #Reshape train and test data
         trainData = trainData.reshape(-1)
         
+        #Do Not smooth out testData
+        testData = scaler.transform(testData).reshape(-1)
+
         #DO Exponential Moving Average Smoothing
         #The data will have a smoother curve than the original noisy data
         ema = 0.0
         gamma = 0.1
-        for ti in range(6500):
+        for ti in range(7000):
             ema = gamma * trainData[ti] + (1-gamma)*ema
             trainData[ti] = ema
 
-        testData = scaler.transform(testData).reshape(-1)#Do Not smooth out testData
         allMidData = np.concatenate([trainData,testData],axis=0)
         return allMidData
 
@@ -96,13 +102,13 @@ if __name__=='__main__':
     '''use this in every class in order to test them separately'''
     lrLearner = LinRegLearner()
     
-    trainSet = lrLearner.train()
-    testSet = lrLearner.test()
-    scaling = lrLearner.scaling()
+    trainSet = lrLearner.train_data()
+    testSet = lrLearner.test_data()
+    lrLearner.data_scaling()
               
-    print('Train:',trainSet)
-    print('Test:',testSet)
-    print('Scaled Mid Data:',scaling)
+#    print('Train:',trainSet)
+#    print('Test:',testSet)
+#    print('Scaled Mid Data:',data_scaling)
 #lrLearner.eofSmoorhing(dataImport)
 
 
