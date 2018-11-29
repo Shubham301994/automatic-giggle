f913e3b2632fc022b917aa7084b1144276ca10d7
David Guerrero <dguerrero23@gmail.com>
2018-11-29 13:47:14 -0500

    Final Revision

--- DataImport.py
+++ DataImport.py
@@ -1,60 +1,34 @@
-'''
-==============================================================================
-This module Imports Adjusted Close Prices for a ticker calculates a moving
-average, standard, sets an upper band and lower band. This data is all plotted
-
-Use this data to have an algorithm decide when to sell or buy stocks based on
-an upper and lower bands.
-
-To Do:
-Create another module that takes this data and uses functions from Robinhood
-API that makes transactions
-==============================================================================
-'''
 import pandas as pd
 from pandas_datareader import data as web
-import matplotlib.pyplot as plt
 
 class DataImport:
-
+    '''==============================================================================
+        This module is used for importing stock data using the Pandas Data Reader
+        which utilizing Yahoo Finnance database for historical stock data. Specific
+        features can be imported as well as a full set of data. 
+    =============================================================================='''
     def pull_feat(self,ticker,start,end,feat):
         '''
-            Uses pandas_datareader to pull Adjusted Close price for a
-            ticker, from start date, to end date. Returns a Pandas DF of
-            AdjCloseDF data. Adj Close is the stock's amdended closing price
-            on any given day of trading to include distributions and corporate
-            actions that occured at any time before the next day's open. Used
-            for examining historical returns
+            Uses pandas_datareader to pull a feature for a ticker (ie. GOOG, AAPL, GE)
+            from a start date to an end date. A feature can be Open Price, High Price, Low
+            Price, Closing Price, Adj Close Price, and Volume. Returns a Pandas DF. These
+            are all defined in the main application TradingWiz.
         '''
-        #These should be imported from main class
-        self.ticker = ticker
-        self.start = start
-        self.end = end
-        
-        self.feature = web.DataReader(self.ticker,data_source='yahoo',start = self.start,end = self.end)[feat]
-        self.featDF = pd.DataFrame(self.feature)#Create the pandas Data Frame
+        self.feature = web.DataReader(ticker,data_source='yahoo',start=start,end=end)[feat]
+        self.featDF = pd.DataFrame(self.feature)
         return self.featDF
     
-    def calcs(self,fb):
+    def pull_DF(self,ticker,start,end):
         '''
-            Takes the Adjusted Close Price
+            Uses pandas_datareader to pull a full data frame that contains all features.
+            Returns a Pandas DF containing all features.
         '''
-        fb['30 Day MA'] = fb['Adj Close'].rolling(window=20).mean()
-        fb['30 Day STD'] = fb['Adj Close'].rolling(window=20).std()
-        fb['Upper Band'] = fb['30 Day MA'] + (fb['30 Day STD'] * 2)
-        fb['Lower Band'] = fb['30 Day MA'] - (fb['30 Day STD'] * 2)
-    
+        self.data = web.DataReader(ticker,data_source='yahoo',start=start,end=end)
+        self.DF = pd.DataFrame(self.data)
+        return self.DF
 
 if __name__=='__main__':
-    '''use this in every class in order to test them separately'''
-    
+    '''Used to test this class separately'''
     dataImport = DataImport()
-    fb = dataImport.pull_feat('ge','1/2/1962', '27/11/2018','High')
-    print(fb.shape)
-
-#dataImport.calcs(fb)
-
-
-
-
-
+    ge = dataImport.pull_DF('ge','1/2/1962', '27/11/2018')
+    print(ge.shape)
